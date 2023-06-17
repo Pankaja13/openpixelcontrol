07532d23d2fe2c42a0f7e2984169a7c8f43af5ba
pankaja <pankaja13.psd@gmail.com>
2023-06-17 11:31:43 +0530

    add channels support

--- layouts/make_trees_circles.py
+++ layouts/make_trees_circles.py
@@ -4,10 +4,10 @@ x_trees = 5
 y_length = 25
 y_trees = 5
 
-strip_length = 15.2
+strip_length = 4.87
 leds_per_strip = 300
 
-circle_radius = 1
+circle_radius = 0.5
 z_height = 2
 
 ###########################################
@@ -23,7 +23,7 @@ y_offset = y_spacing/2
 circumference = 2 * math.pi * circle_radius
 leds_in_circle = int(circumference * leds_per_strip/strip_length)
 
-pos_data = []
+channels = []
 
 
 def make_circle(x_center, y_center):
@@ -38,14 +38,13 @@ def make_circle(x_center, y_center):
 	return points
 
 
-def print_pos_data():
-	for coordinate in pos_data:
-		print(coordinate[0], '\t', coordinate[1])
+def make_json_line(coordinate):
+	return '{"point": [%.2f, %.2f, %.2f]}' % (coordinate[0], coordinate[1], coordinate[2])
 
 
-def write_to_file(filename):
+def write_to_file(filename, channel_data):
 	string = "[\n"
-	lines = ['{"point": [%.2f, %.2f, %.2f]}' % (coordinate[0], coordinate[1], coordinate[2]) for coordinate in pos_data]
+	lines = [make_json_line(coordinate) for coordinate in channel_data]
 	string += ',\n'.join(lines) + '\n]'
 
 	with open(f"{filename}.json", "w") as f:
@@ -56,8 +55,9 @@ for y_tree in range(0, y_trees):
 	y_tree_c = (y_tree * y_spacing) + y_offset - (y_length/2)
 	for x_tree in range(0, x_trees):
 		x_tree_c = (x_tree * x_spacing) + x_offset - (x_length/2)
-		pos_data += make_circle(x_tree_c, y_tree_c)
+		this_channel = make_circle(x_tree_c, y_tree_c)
+		channels.append(this_channel)
 
 
-write_to_file('trees_circles')
-print(f"Total Pixels: {len(pos_data)}")
+for index, channel in enumerate(channels):
+	write_to_file(f'trees_circles_{index+1}', channel)
