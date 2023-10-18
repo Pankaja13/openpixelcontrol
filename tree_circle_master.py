a7b85e32c075c14ed962fbf936292b470103c01c
pankaja <pankaja13.psd@gmail.com>
2023-10-18 14:43:28 +0530

    add timing output

--- tree_circle_master.py
+++ tree_circle_master.py
@@ -3,27 +3,36 @@ import time
 from python.config import trees_config
 from python.mode_manager import Modes, Tree
 from python.utils import flush_all_pixels
+import multiprocessing
 
 if __name__ == '__main__':
 
 	trees_data = []
 
 	for this_tree_config in trees_config:
-		trees_data.append(Tree(Modes.RAIN, this_tree_config['host'], this_tree_config['channel']))
+		trees_data.append(Tree(Modes.JUMP, this_tree_config['host'], this_tree_config['channel']))
 
 	try:
 		while True:
+			list_start = time.time()
 			for number, tree in enumerate(trees_data):
+				start = time.time()
 				# for each tree
 				pixels = tree.get_pixels()
 				if pixels:
-					# print(number)
+
 					tree.send_data(pixels)
+
+				fps = 1 / (time.time() - start)
+				print(f"fps: {int(fps)}")
+			print(time.time() - list_start)
+			time.sleep(0.001)
+
 			# break
 
 	finally:
 		# pass
 		for tree in trees_data:
-			for _ in range(3):
-				flush_all_pixels(tree.send_data)
-				time.sleep(0.1)
+			# for _ in range(3):
+			flush_all_pixels(tree.send_data)
+			# time.sleep(0.1)
