fdf0a512464b46d08d9428fbeb2e4f51f1135f68
pankaja <pankaja13.psd@gmail.com>
2023-11-03 00:33:10 +0530

    update multiple_run.sh

---
+++ multiple_run.sh
@@ -0,0 +1,33 @@
+#poetry run python tree_circle_master.py
+
+
+poetry run python -u tree_circle_master.py -H 10.42.0.2:7891 -p 6002 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.3:7891 -p 6003 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.4:7891 -p 6004 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.5:7891 -p 6005 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.6:7891 -p 6006 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.7:7891 -p 6007 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.8:7891 -p 6008 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.9:7891 -p 6009 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.10:7891 -p 6010 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.11:7891 -p 6011 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.12:7891 -p 6012 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.13:7891 -p 6013 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.14:7891 -p 6014 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.15:7891 -p 6015 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.16:7891 -p 6016 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.17:7891 -p 6017 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.18:7891 -p 6018 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.19:7891 -p 6019 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.20:7891 -p 6020 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.21:7891 -p 6021 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.22:7891 -p 6022 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.23:7891 -p 6023 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.24:7891 -p 6024 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.25:7891 -p 6025 2>&1 &
+poetry run python -u tree_circle_master.py -H 10.42.0.26:7891 -p 6026 2>&1 &
+
+
+
+echo "done!"
+#pkill -f tree_circle
--- run_tree_cmdline.sh
+++
@@ -1,2 +0,0 @@
-#poetry run python tree_circle_master.py
-poetry run python tree_circle_master.py -H 127.0.0.1:7890 -p 6001
